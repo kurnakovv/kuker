66dccfde150ea37a0a33c3001833bc0e14eeb2ac
kurnakovv <maksk210@gmail.com>
2025-12-28 11:10:05 +0900

    Fix build (#1)

--- src/Kuker.Analyzers/Test.cs
+++ src/Kuker.Analyzers/Test.cs
@@ -1,11 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿// Copyright (c) 2025 kurnakovv
+// This file is licensed under the MIT License.
+// See the LICENSE file in the project root for full license information.
 
 namespace Kuker.Analyzers
 {
+    /// <summary>
+    /// Test.
+    /// </summary>
     public class Test
     {
-
     }
 }
